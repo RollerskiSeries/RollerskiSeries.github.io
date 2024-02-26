8dde1ffe40c33a64005d2edddde18ec3f3ebd9b7
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-02-26 15:58:06 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,21 +2,21 @@ $().ready(function(){
     $.getJSON( "/assets/races.json", function( raceData ) {
 	    
     //append the race details
-   const racesElement = document.createElement("div");
+   var racesElement = document.createElement("div");
    for (var race of raceData.races) 
        {
          //create an entry for each race
-         const el = document.createElement("div");
+         var el = document.createElement("div");
          el.setAttribute('id', race.raceID);
-         const h1 = document.createElement("H1");
+         var h1 = document.createElement("H1");
          h1.textContent = race.raceName;
-         const p = document.createElement("P");
+         var p = document.createElement("P");
          p.textContent = race.raceDetails;
          el.appendChild(h1);
          el.appendChild(p);
 	 racesElement.appendChild(el);
        }
 	//update the html
- 	$("#raceList").html(racesElement);
+ 	$("#raceList").append(racesElement);
   });
 });

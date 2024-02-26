8ecfaeca7a14d49d2ee9f3f384b8a72c0726f285
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-02-26 16:37:10 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,24 +1,11 @@
 $().ready(function(){
-    $.getJSON( "/assets/races.json", function( raceData ) {
-
-	$("#raceList").append(raceData);
-	    
-	    //append the race details
-	//var racesElement = document.createElement("div");
-	  // $for (var race of raceData.races) 
-	//	{
-	//	 //create an entry for each race
-	//	 var el = document.createElement("div");
-	//	 el.setAttribute('id', race.raceID);
-	//	 var h1 = document.createElement("H1");
-	//	 h1.textContent = race.raceName;
-	//	 var p = document.createElement("P");
-	//	 p.textContent = race.raceDetails;
-	//	 el.appendChild(h1);
-	//	 el.appendChild(p);
-	//	 racesElement.appendChild(el);
-	//	}
-	        //update the html
-	  //      $("#raceList").append("Hello World2");
-  	});
-});
+        $.getJSON("/assets/races.json",function(data){
+            console.log('success');
+            $.each(data.races.race,function(i,race){
+                $('#raceList').append('<h1>'+race.raceName+'</h1>');
+		$('#raceList').append('<p>'+race.raceDetails+'</p>');
+            });
+        }).error(function(){
+            console.log('error');
+        });
+    });

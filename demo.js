c4e91f2a320566da26c68a17b6447ac21bd92ec1
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-02-26 16:59:49 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,9 +1,12 @@
 $(function(){
         $.getJSON("/assets/races.json",function(data){
+	$('#raceList').append('<h1>Hello World</h1>');
+	//$('#raceList').append('<h1>'+race.raceName+'</h1>');
+	//$('#raceList').append('<p>'+race.raceDetails+'</p>');
             console.log('success');
-            $.each(data.races.race,function(i,race){
-        	$('#raceList').append('<h1>'+race.raceName+'</h1>');
-		$('#raceList').append('<p>'+race.raceDetails+'</p>');
-        	});
+          //  $.each(data.races.race,function(i,race){
+        //	$('#raceList').append('<h1>'+race.raceName+'</h1>');
+	//	$('#raceList').append('<p>'+race.raceDetails+'</p>');
+        //	});
 	});
 });

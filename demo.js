9fc1dd4a44f115e097eb2dccc3703432c89fc129
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-02-26 16:24:10 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,24 +1,24 @@
 $().ready(function(){
     $.getJSON( "/assets/races.json", function( raceData ) {
 
-	$("#raceList").append("Hello World1");
+	$("#raceList").append(raceData);
 	    
 	    //append the race details
-	var racesElement = document.createElement("div");
-	   $for (var race of raceData.races) 
-		{
-		 //create an entry for each race
-		 var el = document.createElement("div");
-		 el.setAttribute('id', race.raceID);
-		 var h1 = document.createElement("H1");
-		 h1.textContent = race.raceName;
-		 var p = document.createElement("P");
-		 p.textContent = race.raceDetails;
-		 el.appendChild(h1);
-		 el.appendChild(p);
-		 racesElement.appendChild(el);
-		}
+	//var racesElement = document.createElement("div");
+	  // $for (var race of raceData.races) 
+	//	{
+	//	 //create an entry for each race
+	//	 var el = document.createElement("div");
+	//	 el.setAttribute('id', race.raceID);
+	//	 var h1 = document.createElement("H1");
+	//	 h1.textContent = race.raceName;
+	//	 var p = document.createElement("P");
+	//	 p.textContent = race.raceDetails;
+	//	 el.appendChild(h1);
+	//	 el.appendChild(p);
+	//	 racesElement.appendChild(el);
+	//	}
 	        //update the html
-	        $("#raceList").append("Hello World2");
+	  //      $("#raceList").append("Hello World2");
   	});
 });

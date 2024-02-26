ec604dca1ed5e2ce349b14b5e908fc2efbf0735e
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-02-26 18:17:55 +0100

    Update races.js

--- races.js
+++ races.js
@@ -1,32 +1,37 @@
 var raceData = {
     "raceYear": 2024,
     "races": [
-        {
-          "raceID": "race1",
-          "raceName": "Race 1",
-          "raceDate": 2024,
-          "raceDetails": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        },
-      
-       {
-          "raceID": "race2",
-          "raceName": "Race 2",
-          "raceDate": 2024,
-          "raceDetails": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        },
+	{
+		"raceVenue": "Hayes",
+		"raceOrganiser": "LRNSC", 
+		"raceName": "12 km time trial – FT",
+		"raceDate": "Sun 14 April",
+		"raceNotes": "Starts at 11:00. Marwe 610s/US0 wheels or no faster (6/3 km junior/novice races)."
+	},
 
-       {
-          "raceID": "race3",
-          "raceName": "Race 3",
-          "raceDate": 2024,
-          "raceDetails": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        }
-      
+	{
+		"raceVenue": "Huntly",
+		"raceOrganiser": "HNSC", 
+		"raceName": "Huntly Sprints – FT",
+		"raceDate": "Sat 25 May",
+		"raceNotes": "GB Series race will be combined time of two time-trials. K.O. sprints in the afternoon. Races start 10:00. Organiser will supply Marwe roller skis."
+	},
+		
+	{
+		"raceVenue": "Rhynie A941",
+		"raceOrganiser": "HNSC", 
+		"raceName": "Clashindarroch Hill Climb – CT",
+		"raceDate": "Sun 26 May",
+		"raceNotes": "Minimum age 11. Any type of classic roller, must have ratchet. 8.05/4.85 km climbs. British Championship (TBC)"
+	},
     ]
 }
 
 //DO NOT EDIT BELOW HERE!!!
 $.each(raceData.races,function(i,race){
         $('#raceList').append('<h1>'+race.raceName+'</h1>');
-		$('#raceList').append('<p>'+race.raceDetails+'</p>');
+	$('#raceList').append('<h3>'+race.raceDate+'</h3>');
+	$('#raceList').append('<h5>'+race.raceVenue+'</h5>');
+	$('#raceList').append('<h5>'+race.raceOrganiser+'</h5>');
+	$('#raceList').append('<p>'+race.raceDetails+'</p>');
    });

e7c9e5ebbd7f79f43d1be20bc264d25e00224125
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-02-26 15:36:56 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,3 +1,24 @@
-$().ready(function() {
-   $("#raceList").html("Text added by jQuery code.");
+$().ready(function(){
+    $.getJSON( "/assets/races.json", function( raceData ) {
+          
+   var doc = document.getElementById("raceList");
+   
+    //remove the placeholder text
+	doc.innerHTML = "";
+    
+    //append the race details
+   for (var race of raceData.races) 
+       {
+         //create an entry for each race
+         const el = document.createElement("div");
+         el.setAttribute('id', race.raceID);
+         const h1 = document.createElement("H1");
+         h1.textContent = race.raceName;
+         const p = document.createElement("P");
+         p.textContent = race.raceDetails;
+         el.appendChild(h1);
+         el.appendChild(p);
+         doc.appendChild(el);
+       }
+  });
 });

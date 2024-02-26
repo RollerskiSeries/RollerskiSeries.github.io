7909b2e3653827286af72b0da38d1bb142384a84
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-02-26 16:12:54 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -4,18 +4,18 @@ $().ready(function(){
     //append the race details
    var racesElement = document.createElement("div");
    $for (var race of raceData.races) 
-       {
-         //create an entry for each race
-         var el = document.createElement("div");
-         el.setAttribute('id', race.raceID);
-         var h1 = document.createElement("H1");
-         h1.textContent = race.raceName;
-         var p = document.createElement("P");
-         p.textContent = race.raceDetails;
-         el.appendChild(h1);
-         el.appendChild(p);
-         racesElement.appendChild(el);
-       }
+	{
+	 //create an entry for each race
+	 var el = document.createElement("div");
+	 el.setAttribute('id', race.raceID);
+	 var h1 = document.createElement("H1");
+	 h1.textContent = race.raceName;
+	 var p = document.createElement("P");
+	 p.textContent = race.raceDetails;
+	 el.appendChild(h1);
+	 el.appendChild(p);
+	 racesElement.appendChild(el);
+	}
         //update the html
         $("#raceList").append(racesElement);
   });

20c7ce7fb2f0c85255ddc1488692032e70b6a2ef
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-02-27 14:24:14 +0100

    Update races.js

--- races.js
+++ races.js
@@ -1,3 +1,7 @@
+var title = "GB Rollserski Series 2024"
+
+var intro = "Lorem ipsum dolor sit amet"
+
 var raceData = {
     "raceYear": 2024,
     "races": [
@@ -31,13 +35,30 @@ var raceData = {
 }
 
 //DO NOT EDIT BELOW HERE!!!
+
+//Add Title and intro text div
+$('#header').append('<h1 style = "color:rgb(0,56,101)" class="w3-jumbo"><b>'+title+'</b></h1>')
+$('#header').append('<div class="w3-container" id="introText" style="margin-top:75px"></div>')
+
+//Add intro text
+$('#introText').append('<p>'+intro+'</p>')
+
+// Read the race data
 $.each(raceData.races,function(i,race){
+        
+        //Create a div and add a space
         $('#raceList').append('<div id ="'+race.raceID+'"></div>');
 	$('#'+race.raceID).append('<br>');
+    // Put the date in
         $('#'+race.raceID).append('<h3>'+race.raceDate+'</h3>');
+    //give it a title based on the race name
         $('#'+race.raceID).append('<h1>'+race.raceName+'</h1>');
+    // add the organising club and location
 	$('#'+race.raceID).append('<h3>'+race.raceVenue+' - '+race.raceOrganiser+'</h3>');
+    //add notes and another space
 	$('#'+race.raceID).append('<p>'+race.raceNotes+'</p>');
 	$('#'+race.raceID).append('<br>');
+    
+    //Put an entry in the menu
 	$('#racesMenu').append('<dd><a style="color: white;"href="#'+race.raceID+'" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">'+race.raceName+'</a></dd>');  
    });

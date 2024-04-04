8660fb2c0b4c233d57689a418900ad20d75c4d0f
pthomson88 <62576469+pthomson88@users.noreply.github.com>
2024-04-04 22:09:01 +0200

    Update races.js
    make it red

--- races.js
+++ races.js
@@ -127,7 +127,7 @@ $.each(rulePoints,function(i, val){
 });
 
 // Add Championship race details
-$('#championshipRace').append('<h1 style = "color:rgb(0,56,101)"><a href="#champs" style="text-decoration:none">'+championshipRaceHeader+'</a></h1>')
+$('#championshipRace').append('<h1 style = "color:rgb(203,51,59)"><a href="#champs" style="text-decoration:none">'+championshipRaceHeader+'</a></h1>')
 $('#championshipRace').append('<p>'+championshipRaceBlurb+'</p>')
 
 
